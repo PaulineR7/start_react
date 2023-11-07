59a71eef94b972d5605339b3dcae61ac19521bd2
Pauline Remi <pauline.remi@lapiscine.pro>
2023-11-07 10:42:49 +0100

    react start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+
+const h1 = "Hello la piscine !";
+const paragraph = "Salut je m'apelle Pauline, je suis en formation à la piscine";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> {h1} </h1>
+      <p> {paragraph} </p>
     </div>
   );
 }
